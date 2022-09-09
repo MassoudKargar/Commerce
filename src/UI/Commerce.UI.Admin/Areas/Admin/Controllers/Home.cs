1940eb6348d6b381cfdc5bed7f9b456febd9e481
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2022-09-09 11:45:09 -0700

    Add Admin Html

---
+++ src/UI/Commerce.UI.Admin/Areas/Admin/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Commerce.UI.Admin.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class Home : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
